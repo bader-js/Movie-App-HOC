8b46c37d09f6a0d53948e0c34d4c97cc7c9b8f03
Mohamed Bader Ben Rejeb <sky.bader@gmail.com>
2019-11-20 12:09:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieContainer from './components/movieContainer.js';
-
-
+import WithLoading from './components/WithLoading'
+const Loading = Component => {
+  return ({ isLoading }) => {
+    if (!isLoading) return <Component />;
+    return <div className="loader spinner-border text-success" />;
+  };
+};
+const Content = Loading(MovieContainer);
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false }),2000);
+  }
   render() {
     return (
-      <div className="App">
-        <MovieContainer />
+      <div className="App ">
+        <Content isLoading={this.state.loading} />
       </div>
     );
   }
 }
 
-export default App;
+
+export default WithLoading(App);
---
+++ src/components/WithLoading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+function WithLoading(Component) {
+    return function WihLoadingComponent({ isLoading, ...props }) {
+      if (!isLoading) return (<Component {...props} />);
+    }
+}
+
+
+      export default WithLoading;
